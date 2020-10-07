cd698449e560df12832ef8b99f27f4141ce702ef
Edward Masih <masih.edward45@gmail.com>
2020-10-08 01:43:46 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import TodoItem from './todo-component/todo';
-import todosData from './todo-data';
-
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      todos: todosData,
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  
-  handleChange(id){
-    this.setState(prevState => {
-        const updatedTodos = prevState.todos.map(todo => {
-          if (todo.id === id) {
-            todo = {...todo, completed: !todo.completed}  //used spread operator
-          }
-          return todo
-        })
-
-        console.log("Clicked ", id)
-
-        return ({
-          todos: updatedTodos,
-        })
-    })
-  }
-
-  render(){
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+function App () {
 
     return (
       <div className="todo-list">
-          {todoItems}
+          Hi
       </div>
     );
-  };
+  
 }
 
 export default App;
